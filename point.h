7bb858a23be4c51ae34cdbfe97ed78ba8834d51f
Poopenstein <thedoctorisinnow@gmail.com>
2018-10-22 23:25:29 -0500

    Update point.h

--- point.h
+++ point.h
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 #ifndef POINT_H
 #define POINT_H
 struct Point {
 	double x_coord;
 	double y_coord;
-=======
-#ifndef POINT_H
-#define POINT_H
-struct Point {
-	double x_coord;
-	double y_coord;
->>>>>>> 0adddffe1f3e0da0ccd617a3e6287d0f28eeae08
 };
+//rather than saying 'struct Point' we can say 's_Point'
+typedef struct Point s_Point;
+#endif
