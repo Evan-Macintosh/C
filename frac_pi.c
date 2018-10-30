4e29600f541e2b9fad1acd42b43fb2c24aba4f5d
Poopenstein <thedoctorisinnow@gmail.com>
2018-10-29 23:10:41 -0500

    Added register usage for variables

--- frac_pi.c
+++ frac_pi.c
@@ -2,16 +2,16 @@
 #include <math.h>
 #include <stdlib.h>
 #include <float.h>
-
-int main (){
-	unsigned long max=9223372036854775807; //2^63
-	unsigned long n, denom=1;
-	float pi = 1.0;
+#define END 9223372036854775807 //2^63
+void main (){
+	register unsigned long max=END; //2^63
+	register unsigned long n=1 
+	register unsigned long denom=3;
+	register float pi = 1.0;
 	
-	do{
+	while(denom < max){
 		pi += (pow(-1, n++))/denom;
 		denom +=2;
-	}while(denom < max);
+	}
 	printf("pi = %.20f\n",pi*4);
-	return 0;
 }
